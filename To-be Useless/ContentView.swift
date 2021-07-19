2dcac30ee137a88f2936d6f685272ae6f5dc27f7
viere1234 <69096696+viere1234@users.noreply.github.com>
2021-07-19 08:21:40 +0800

    Initial Commit

---
+++ To-be Useless/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To-be Useless
+//
+//  Created by 張智堯 on 2021/7/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To-be Useless/To_be_UselessApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_be_UselessApp.swift
+//  To-be Useless
+//
+//  Created by 張智堯 on 2021/7/19.
+//
+
+import SwiftUI
+
+@main
+struct To_be_UselessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
